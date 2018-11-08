69c238f6d89836539c76eb7be1f9af8ba673b12f
mappingvermont <charlie.hofmann@gmail.com>
2018-11-08 11:57:13 -0500

    update README, add glad analysis.type

--- local.sh
+++ local.sh
@@ -4,5 +4,3 @@ rm -r data/output/
 spark-submit\
  target/spark-pip-0.3.jar\
  local.properties
-
-./read_local_output.py
--- src/main/scala/com/esri/Summarize.scala
+++ src/main/scala/com/esri/Summarize.scala
@@ -16,6 +16,7 @@ object Summary {
       case "loss" => processLoss(with_poly)
       case "biomass" => processBiomass(with_poly)
       case "fire" => processFire(with_poly)
+      case "glad" => processGLAD(with_poly)
       case _ => throw new IllegalArgumentException
     }
 
@@ -34,7 +35,8 @@ object Summary {
     inRDD.map({case Array(thresh, area, polyname, bound1, bound2, bound3, bound4, iso, id1, id2) =>
               (ExtentRow(polyname, bound1, bound2, bound3, bound4, iso, id1, id2, HansenUtils.matchTest(thresh), area.toDouble)) })
               .toDF()
-              .groupBy("polyname", "bound1", "bound2", "bound3", "bound4", "iso", "id1", "id2", "thresh").agg(sum("area"))
+              .groupBy("polyname", "bound1", "bound2", "bound3", "bound4", "iso", "id1", "id2", "thresh")
+              .agg(sum("area"))
     }
 
   def processLoss(inRDD: RDD[Array[String]])(implicit sqlContext: SQLContext): DataFrame = {
@@ -44,7 +46,8 @@ object Summary {
               (LossRow(polyname, bound1, bound2, bound3, bound4, iso, id1, id2, 
                        year, area.toDouble, HansenUtils.matchTest(thresh), HansenUtils.biomass_per_pixel(biomass)(area))) })
               .toDF()
-              .groupBy("polyname", "bound1", "bound2", "bound3", "bound4", "iso", "id1", "id2", "thresh", "year").agg(sum("area"), sum("biomass"))
+              .groupBy("polyname", "bound1", "bound2", "bound3", "bound4", "iso", "id1", "id2", "thresh", "year")
+              .agg(sum("area"), sum("biomass"))
     }
 
   def processBiomass(inRDD: RDD[Array[String]])(implicit sqlContext: SQLContext): DataFrame = {
@@ -54,7 +57,8 @@ object Summary {
               (BiomassRow(polyname, bound1, bound2, bound3, bound4, iso, id1, id2, 
                           HansenUtils.biomass_per_pixel(raw_biomass)(area))) })
               .toDF()
-              .groupBy("polyname", "bound1", "bound2", "bound3", "bound4", "iso", "id1", "id2").agg(sum("biomass"))
+              .groupBy("polyname", "bound1", "bound2", "bound3", "bound4", "iso", "id1", "id2")
+              .agg(sum("biomass"))
     }
 
   def processFire(inRDD: RDD[Array[String]])(implicit sqlContext: SQLContext): DataFrame = {
@@ -63,7 +67,19 @@ object Summary {
     inRDD.map({case Array(lat, lon, acq_date, fire_type, polyname, bound1, bound2, bound3, bound4, iso, id1, id2) =>
               (FireRow(acq_date, fire_type, polyname, bound1, bound2, iso, id1, id2 )) })
               .toDF()
-              .groupBy("acq_date", "fire_type", "polyname", "bound1", "bound2", "iso", "id1", "id2").count()
+              .groupBy("acq_date", "fire_type", "polyname", "bound1", "bound2", "iso", "id1", "id2")
+              .count()
+    }
+
+  def processGLAD(inRDD: RDD[Array[String]])(implicit sqlContext: SQLContext): DataFrame = {
+
+    import sqlContext.implicits._
+    inRDD.map({case Array(lat, lon, confidence, year, julian_day, area_m2, emissions, climate_mask, polyname, bound1, bound2, iso, id1, id2) =>
+              (GLADRow(confidence, year, julian_day, climate_mask, polyname, bound1, bound2, iso, id1, id2, area_m2.toDouble, emissions.toDouble )) })
+              .toDF()
+              .groupBy("confidence", "year", "julian_day", "climate_mask", "polyname", "bound1", "bound2", "iso", "id1", "id2")
+              .count()
+              .agg(sum("area_m2"), sum("emissions"))
     }
 
   case class ExtentRow( polyname: String, bound1: String, bound2: String, bound3: String, bound4: String, 
@@ -77,5 +93,9 @@ object Summary {
   
   case class FireRow( acq_date: String, fire_type: String, polyname: String, bound1: String, bound2: String, 
                       iso: String, id1: String, id2: String )
+
+  case class GLADRow( confidence: String, year: String, julian_day: String, climate_mask: String, polyname: String, 
+                      bound1: String, bound2: String, iso: String, id1: String, id2: String, area_m2: Double, emissions: Double )
+
 
 }
